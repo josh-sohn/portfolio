18b007967a8d1dc00a01d3366ca2e7ef62ff461a
sohnOfGod <jsohn000@gmail.com>
2018-09-28 14:56:52 -0700

    nothing

---
+++ php_mailer/email_config.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','servermailersmurf@gmail.com');
+define('EMAIL_PASS','s3rv3rPath1');
+define('EMAIL_USERNAME','From Smurf Email');
+define('EMAIL_TO_ADDRESS','jsohn000@gmail.com');
+?>
